75435163d529c389606bacdd76332e802f51c41b
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2025-01-17 21:04:51 +0200

    initial project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export const metadata = {
+  title: 'Home',
+};
+
+const Component = () => {
+  return <Button>Hello</Button>;
+};
+
+export default Component;
