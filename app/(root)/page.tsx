251282835b5177081c1e4f21627db580772e4b60
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2025-01-18 23:42:12 +0200

    add product cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from '@/components/ui/button';
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
 export const metadata = {
   title: 'Home',
 };
 
-const Component = async () => {
-  return <Button>Hello</Button>;
+const Homepage = async () => {
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
-export default Component;
+export default Homepage;
---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils';
+
+interface Props {
+  value: number;
+  className?: string;
+}
+
+const ProductPrice = ({ value, className }: Props) => {
+  const stringValue = value.toFixed(2);
+  const [intValue, floatValue] = stringValue.split('.');
+  return (
+    <p className={cn('text-2xl', className)}>
+      <span className="text-xs align-super">$</span>
+      {intValue}
+      <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  );
+};
+
+export default ProductPrice;
