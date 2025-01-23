1d4f90bccbb017c904ec09402e55005e54c19859
Pavlo Lytvyshko <pavlo.litvishko@gmail.com>
2025-01-23 16:10:51 +0200

    add sign in functionality

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,58 @@
+import { Metadata } from "next";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+import CredentialsSignForm from "./credentials-sign-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+interface Props {
+  searchParams: Promise<{ callbackUrl: string }>;
+}
+
+const SignInPage = async (props: Props) => {
+  const { callbackUrl } = await props.searchParams;
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Card>
+        <CardHeader className="space-y-4">
+          <Link href="/" className="flex-center">
+            <Image
+              src="/images/logo.svg"
+              width={100}
+              height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-center">
+            Sign in to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <CredentialsSignForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SignInPage;
